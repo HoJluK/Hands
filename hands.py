7808e4a0d0671c7e70ea9cfff45e9830ab600b37
Vladislav Alekseev <55919069+HoJluK@users.noreply.github.com>
2023-10-20 13:07:50 +0300

    Add files via upload

---
+++ hands.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import re
+
+def extract_phone_numbers(url):
+    driver = webdriver.Chrome()  # Укажите путь к драйверу вашего браузера
+    driver.get(url)
+    time.sleep(2)  # Подождите, чтобы страница полностью загрузилась
+
+    phone_numbers = set()  # Множество для хранения уникальных номеров
+
+    phone_pattern = re.compile(r'\d{1}\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}')
+    matches = phone_pattern.findall(driver.page_source)
+
+    for match in matches:
+        phone_numbers.add(match)
+
+    for number in phone_numbers:
+        print(number)
+
+    driver.quit()
+
+# Пример использования:
+if __name__ == "__main__":
+    extract_phone_numbers("https://hands.ru/company/about")
+    extract_phone_numbers("https://repetitors.info")
